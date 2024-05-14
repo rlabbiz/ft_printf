4b8cb34a5a9c2f9d51910554142c6b1bde9cd6bd
rida labbiz <redalabbiz@gmail.com>
2024-05-14 11:27:41 +0100

    change header file in source files

--- sources/ft_printf.c
+++ sources/ft_printf.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "../includes/ft_printf.h"
 
 void	ft_check(va_list ap, int c, int *len)
 {
--- sources/ft_putadd.c
+++ sources/ft_putadd.c
@@ -10,7 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_printf.h"
+#include "../includes/ft_printf.h"
 
 void	ft_puthex_(unsigned long long nbr, int *len)
 {
